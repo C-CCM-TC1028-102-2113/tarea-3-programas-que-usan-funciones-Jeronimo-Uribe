ed116a34292dae5012bdec47f0e3ca7d7481cc58
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-08-30 19:30:11 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,13 +1,12 @@
-
+def Tarjetas(plumones,pliegos):
+    tpp=(plumones*35)
+    tppa=(pliegos*12)
+    if tpp>tppa:
+        print('El número de tarjetas que se puede hacer es: %i' %tppa)
+    elif tppa>tpp:
+        print('El número de tarjetas que se puede hacer es: %i' %tpp)
 def main():
     #escribe tu código abajo de esta línea
-    def Tarjetas(plumones,pliegos):
-        tpp=(plumones*35)
-        tppa=(pliegos*12)
-        if tpp>tppa:
-            print('El número de tarjetas que se puede hacer es: %i' %tppa)
-        elif tppa>tpp:
-            print('El número de tarjetas que se puede hacer es: %i' %tpp)
 
     pliegospapel=int(input('Dame la cantidad de pliegos de papel albanene: '))
     plumones=int(input('Dame la cantidad de plumones: '))
